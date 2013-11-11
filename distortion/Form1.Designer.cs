7ac4613560ecdc4240597237c8e4d721fedf6adc
nakanomi <nakanomi@gmail.com>
2013-11-11 19:44:37 +0900

    ローカルから持ってきたところ 円を描くための処理を作成中

---
+++ distortion/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace distortion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxTestSource = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxDistortionTexture = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBoxTestDist = new System.Windows.Forms.PictureBox();
+            this.buttonLoadTest = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoadDistortion = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ScrollBarMag = new System.Windows.Forms.HScrollBar();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.hScrollBarRadius = new System.Windows.Forms.HScrollBar();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTestSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDistortionTexture)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTestDist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBoxTestSource);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 260);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBoxTestSource
+            // 
+            this.pictureBoxTestSource.Location = new System.Drawing.Point(2, 2);
+            this.pictureBoxTestSource.Name = "pictureBoxTestSource";
+            this.pictureBoxTestSource.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxTestSource.TabIndex = 0;
+            this.pictureBoxTestSource.TabStop = false;
+            this.pictureBoxTestSource.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxTestSource_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBoxDistortionTexture);
+            this.panel1.Location = new System.Drawing.Point(278, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 260);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBoxDistortionTexture
+            // 
+            this.pictureBoxDistortionTexture.Location = new System.Drawing.Point(2, 2);
+            this.pictureBoxDistortionTexture.Name = "pictureBoxDistortionTexture";
+            this.pictureBoxDistortionTexture.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxDistortionTexture.TabIndex = 0;
+            this.pictureBoxDistortionTexture.TabStop = false;
+            this.pictureBoxDistortionTexture.Click += new System.EventHandler(this.pictureBoxDistortionTexture_Click);
+            this.pictureBoxDistortionTexture.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxDistortionTexture_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBoxTestDist);
+            this.panel3.Location = new System.Drawing.Point(544, 14);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(260, 260);
+            this.panel3.TabIndex = 3;
+            // 
+            // pictureBoxTestDist
+            // 
+            this.pictureBoxTestDist.Location = new System.Drawing.Point(2, 2);
+            this.pictureBoxTestDist.Name = "pictureBoxTestDist";
+            this.pictureBoxTestDist.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxTestDist.TabIndex = 0;
+            this.pictureBoxTestDist.TabStop = false;
+            this.pictureBoxTestDist.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxTestDist_Paint);
+            // 
+            // buttonLoadTest
+            // 
+            this.buttonLoadTest.Location = new System.Drawing.Point(14, 278);
+            this.buttonLoadTest.Name = "buttonLoadTest";
+            this.buttonLoadTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadTest.TabIndex = 4;
+            this.buttonLoadTest.Text = "ロード";
+            this.buttonLoadTest.UseVisualStyleBackColor = true;
+            this.buttonLoadTest.Click += new System.EventHandler(this.buttonLoadTest_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(370, 278);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "セーブ";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonLoadDistortion
+            // 
+            this.buttonLoadDistortion.Location = new System.Drawing.Point(289, 278);
+            this.buttonLoadDistortion.Name = "buttonLoadDistortion";
+            this.buttonLoadDistortion.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadDistortion.TabIndex = 6;
+            this.buttonLoadDistortion.Text = "ロード";
+            this.buttonLoadDistortion.UseVisualStyleBackColor = true;
+            this.buttonLoadDistortion.Click += new System.EventHandler(this.buttonLoadDistortion_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ScrollBarMag
+            // 
+            this.ScrollBarMag.Location = new System.Drawing.Point(289, 329);
+            this.ScrollBarMag.Name = "ScrollBarMag";
+            this.ScrollBarMag.Size = new System.Drawing.Size(220, 17);
+            this.ScrollBarMag.TabIndex = 7;
+            this.ScrollBarMag.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBarMag_Scroll);
+            this.ScrollBarMag.ValueChanged += new System.EventHandler(this.hScrollBarMag_ValueChanged);
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(289, 407);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 8;
+            this.buttonTest.Text = "buttonTest";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // hScrollBarRadius
+            // 
+            this.hScrollBarRadius.Location = new System.Drawing.Point(289, 375);
+            this.hScrollBarRadius.Name = "hScrollBarRadius";
+            this.hScrollBarRadius.Size = new System.Drawing.Size(220, 17);
+            this.hScrollBarRadius.TabIndex = 9;
+            this.hScrollBarRadius.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBarRadius_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 442);
+            this.Controls.Add(this.hScrollBarRadius);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.ScrollBarMag);
+            this.Controls.Add(this.buttonLoadDistortion);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoadTest);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTestSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDistortionTexture)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTestDist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBoxTestSource;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxDistortionTexture;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBoxTestDist;
+        private System.Windows.Forms.Button buttonLoadTest;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoadDistortion;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.HScrollBar ScrollBarMag;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.HScrollBar hScrollBarRadius;
+    }
+}
+
---
+++ distortion/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace distortion
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmpTestSource = null;
+        Bitmap bmpDistortionTex = null;
+        Bitmap bmpTestDest = null;
+        bool isBusy = false;
+        float mMag = 5.0f;
+        Point mPosCenter = new Point();
+        float mRadius = 64.0f;
+
+        float mCenterVal = 128.0f;
+        float mMaxVal = 255.0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            {
+                bmpDistortionTex = new Bitmap(pictureBoxDistortionTexture.Width, pictureBoxDistortionTexture.Height);
+                bmpTestDest = new Bitmap(pictureBoxTestDist.Width, pictureBoxTestDist.Height);
+                mPosCenter.X = pictureBoxDistortionTexture.Width / 2;
+                mPosCenter.Y = pictureBoxDistortionTexture.Height / 2;
+            }
+        }
+        #region Paint
+        private void pictureBoxTestSource_Paint(object sender, PaintEventArgs e)
+        {
+            if (bmpTestSource != null)
+            {
+                Graphics gr = e.Graphics;
+                gr.DrawImage(bmpTestSource,
+                    0, 0, new Rectangle(0, 0, pictureBoxTestSource.Width, pictureBoxTestSource.Height),
+                    GraphicsUnit.Pixel);
+            }
+        }
+
+
+        private void pictureBoxDistortionTexture_Paint(object sender, PaintEventArgs e)
+        {
+            if (bmpDistortionTex != null)
+            {
+                Graphics gr = e.Graphics;
+                gr.DrawImage(bmpDistortionTex,
+                    0, 0, new Rectangle(0, 0, pictureBoxDistortionTexture.Width, pictureBoxDistortionTexture.Height),
+                    GraphicsUnit.Pixel);
+            }
+
+        }
+
+        private void pictureBoxTestDist_Paint(object sender, PaintEventArgs e)
+        {
+            if (
+                (bmpTestDest != null) &&
+                (bmpDistortionTex != null) &&
+                (bmpTestSource != null)
+                )
+            {
+                isBusy = true;
+                for (int y = 0; y < bmpTestDest.Height; y++)
+                {
+                    for (int x = 0; x < bmpTestDest.Width; x++)
+                    {
+                        Color col = bmpDistortionTex.GetPixel(x, y);
+                        if (col.A == 0)
+                        {
+                            bmpTestDest.SetPixel(x, y, bmpTestSource.GetPixel(x, y));
+                        }
+                        else
+                        {
+
+                            float xOffset = (float)col.R;
+                            xOffset -= mCenterVal;
+                            xOffset /= mMaxVal;
+                            xOffset *= mMag;
+                            int xVal = (int)(xOffset + (float)x);
+
+                            float yOffset = (float)col.G;
+                            yOffset -= mCenterVal;
+                            yOffset /= mMaxVal;
+                            yOffset *= mMag;
+                            int yVal = (int)(yOffset + (float)y);
+
+                            bmpTestDest.SetPixel(x, y, bmpTestSource.GetPixel(xVal, yVal));
+
+                        }
+                    }
+                }
+                Graphics gr = e.Graphics;
+                gr.DrawImage(bmpTestDest, 0, 0,
+                    new Rectangle(0, 0, pictureBoxTestDist.Width, pictureBoxTestDist.Height),
+                    GraphicsUnit.Pixel);
+                isBusy = false;
+            }
+        }
+        #endregion
+
+        private void pictureBoxDistortionTexture_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("aaa");
+        }
+        #region Buttons
+
+        private void buttonLoadTest_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bmpTestSource = new Bitmap(dlg.FileName);
+                pictureBoxTestSource.Invalidate();
+            }
+        }
+
+        private void buttonLoadDistortion_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bmpDistortionTex = new Bitmap(dlg.FileName);
+                pictureBoxDistortionTexture.Invalidate();
+                pictureBoxTestDist.Invalidate();
+            }
+        }
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            drawCircle();
+            pictureBoxTestDist.Invalidate();
+            pictureBoxDistortionTexture.Invalidate();
+        }
+        #endregion
+
+        private void drawCircle()
+        {
+            if (bmpDistortionTex != null) {
+                Graphics gr = Graphics.FromImage(bmpDistortionTex);
+                SolidBrush brushClear = new SolidBrush(Color.FromArgb((int)mMaxVal, (int)mCenterVal, (int)mCenterVal, (int)mCenterVal));
+                gr.FillRectangle(brushClear, new Rectangle(0, 0, bmpDistortionTex.Width, bmpDistortionTex.Height));
+                int xStart = mPosCenter.X - (int)mRadius;
+                xStart = Math.Max(0, xStart);
+                int yStart = mPosCenter.Y - (int)mRadius;
+                yStart = Math.Max(0, yStart);
+                int xEnd = mPosCenter.X + (int)mRadius;
+                xEnd = Math.Min(xEnd, bmpDistortionTex.Width);
+                int yEnd = mPosCenter.Y + (int)mRadius;
+                yEnd = Math.Min(yEnd, bmpDistortionTex.Height);
+                Point posCur = new Point();
+                for (int y = yStart; y < yEnd; y++)
+                {
+                    for (int x = xStart; x < xEnd; x++)
+                    {
+                        posCur.X = x;
+                        posCur.Y = y;
+                        Point vOffset = Point.Subtract(posCur, new Size(mPosCenter));
+                        float dist = ((float)vOffset.X * (float)vOffset.X) + ((float)vOffset.Y * (float)vOffset.Y);
+                        dist = (float)Math.Sqrt(dist);
+                        if (dist < mRadius)
+                        {
+                            float tmp = dist / mRadius;
+                            tmp *= mMaxVal;
+                            bmpDistortionTex.SetPixel(x, y, Color.FromArgb((int)tmp, 0, 0));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void hScrollBarMag_ValueChanged(object sender, EventArgs e)
+        {
+            //Console.WriteLine("bbb");
+        }
+
+        private void hScrollBarMag_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (!isBusy)
+            {
+                mMag = (float)((float)e.NewValue * 0.4);
+                pictureBoxTestDist.Invalidate();
+            }
+        }
+
+        private void hScrollBarRadius_Scroll(object sender, ScrollEventArgs e)
+        {
+            float minRadius = 32.0f;
+            mRadius = (float)(e.NewValue) + minRadius;
+            Console.WriteLine(mRadius);
+        }
+
+    }
+}
